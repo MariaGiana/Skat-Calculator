174f086187a158e2744f4974130d7dde2deb238d
MARIA GIANA <mariajosegiana@gmail.com>
2026-05-21 13:13:41 +0200

    incoming function working,

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 "use strict";
 
+
 //escuchar eventos de click en los botones de modo y mostrar/ocultar el formulario correspondiente
 let salaryEstimate=document.querySelector("#salaryEstimate");
-salaryEstimate.addEventListener("click", calcularSalary);
 
 let requiredHours=document.querySelector("#requiredHours");
 requiredHours.addEventListener("click", calcularHours);
@@ -18,6 +18,11 @@ requiredHours.addEventListener("click", () => {
   mostrarFormulario.style.display = "none";
 })
 
+let form = document.querySelector("#formulario"); 
+form.addEventListener("submit", calculate); 
+
+
+
 const texts = {
   es: {
     salaryEstimate: "Estimación de Salario",
@@ -28,9 +33,9 @@ const texts = {
   taxA: "Primaria A",
   taxB: "Secundaria B"
 },
-    calcular: "Calcular",
     modeDescription: "Calcula el salario mensual despues de impuestos o cuantas horas necesitas trabajar para alcanzar un ingreso objetivo.",
 note: "Nota: los resultados son una estimación basada en un modelo simplificado, dan una idea general del ingreso neto en Dinamarca. Puede variar según situación personal, deducciones y reglas fiscales reales."  
+
 },
   en: {
     salaryEstimate: "Salary Estimate",
@@ -42,7 +47,8 @@ note: "Nota: los resultados son una estimación basada en un modelo simplificado
   taxB: "Secondary B"
 },
     modeDescription: "Estimate your monthly net salary after taxes or calculate how many hours you need to work to reach a target income.",
- note: "Note: the results are an estimation based on a simplified model, giving a general idea of net income in Denmark. It can vary based on personal situation, deductions, and actual tax rules."  
+
+    note: "Note: the results are an estimation based on a simplified model, giving a general idea of net income in Denmark. It can vary based on personal situation, deductions, and actual tax rules."  
 }
 };
 
@@ -60,22 +66,130 @@ function setLanguage(lang) {
   document.querySelector("#labelTaxB").textContent = texts[lang].seleccionar.taxB;
   document.querySelector("#modeDescription").textContent = texts[lang].modeDescription;
   document.querySelector("#note").textContent = texts[lang].note;
-document.querySelector("#salaryEstimate").textContent = texts[lang].salaryEstimate;
-document.querySelector("#requiredHours").textContent = texts[lang].requiredHours;
+  document.querySelector("#salaryEstimate").textContent = texts[lang].salaryEstimate;
+  document.querySelector("#requiredHours").textContent = texts[lang].requiredHours;
 };
 
 
-//funciones para calcular salario y horas requeridas (a implementar)
-
-function calcularSalary() {
-  console.log("Calculating salary...");
+//funciones para calcular  horas requeridas (a implementar)
 
 
-
-};
 
 function calcularHours() {
  
   console.log("Calculating required hours...");
 }
 
+function calculate(event) {
+    event.preventDefault();
+document.querySelector("#resultados").innerHTML = "";
+let data= new FormData(form);
+
+const resultados = document.querySelector("#resultados");
+const am = 0.08;
+const tax = 0.36;
+const ferie = 0.125;
+
+let horas = Number(data.get('horas'));
+let horasTrabajadas = Number(data.get('horasTrabajadas'));
+let fradrag = Number(data.get('fradrag'));
+let tipo = data.get("opcion");
+
+if (!validate(horas, horasTrabajadas, fradrag, tipo)) {
+    return;
+}
+
+
+
+let bruto= horas * horasTrabajadas;
+    console.log("Bruto: " + bruto);
+let afterAM = bruto * (1 - am);
+    console.log("After AM: " + afterAM);
+let neto;
+let final;
+let feriepenge = (afterAM *(1-tax)) * ferie;
+
+if(tipo === "conFradrag"){ 
+    
+  let taxable = afterAM - fradrag;
+    console.log("Taxable amount: " + taxable);
+  if (taxable < 0) taxable = 0;
+     neto = taxable * (1 - tax);
+     console.log("Neto sin fradrag: " + neto);
+     final= neto + (fradrag*(1 - am));
+     console.log("Neto final con fradrag: " + final);
+   
+    
+} else {
+   final = afterAM * (1 - tax);
+     console.log("Neto sin fradrag: " + neto);
+    
+     console.log("Neto final sin fradrag: " + final);
+
+}
+ resultados.innerHTML = `
+  <div class="resultado-card">
+
+    <p class="resultado-item">
+      <span class="label">
+        Estimated monthly net income / Ingreso mensual neto estimado
+      </span>
+
+      <span class="value">
+        ${final.toFixed(2)} DKK
+      </span>
+    </p>
+
+    <p class="resultado-item">
+      <span class="label"> Estimated holiday pay accumulation/ Acumulación estimada de pago de vacaciones
+       
+      </span>
+
+      <span class="value">
+        ${feriepenge.toFixed(2)} DKK
+      </span>
+    </p>
+
+    <button id="btnBorrar">
+      CLEAR
+    </button>
+
+  </div>
+`;
+form.reset();
+
+document.querySelector("#btnBorrar").addEventListener("click", limpiarCampos);
+}
+
+
+function limpiarCampos() {
+document.querySelector("#resultados").innerHTML = "";
+form.reset();
+
+}
+
+function validate(horas, horasTrabajadas, fradrag, tipo) {
+if (isNaN(horas) || horas <= 0) {
+  alert("Por favor, ingresa un número válido de horas por semana.");
+  return false;
+
+}
+if (isNaN(horasTrabajadas) || horasTrabajadas <= 0) {
+  alert("Por favor, ingresa un número válido de horas trabajadas.");
+  return false;
+}
+
+if (isNaN(fradrag) || fradrag < 0) {
+  alert("Por favor, ingresa un número válido para el fradrag.");
+  return false;
+}
+
+if (!tipo) {
+  alert("Por favor, selecciona una opcion de Taxcard.");
+  return false;
+}
+
+return true;
+
+
+}
